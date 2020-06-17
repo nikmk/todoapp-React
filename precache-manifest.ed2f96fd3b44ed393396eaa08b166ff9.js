c155022e3ebebf757a88580cec6df9a514e2c56d
nikmk <41271779+nikmk@users.noreply.github.com>
2020-06-17 22:51:13 +0530

    Updates

---
+++ precache-manifest.ed2f96fd3b44ed393396eaa08b166ff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089ab5f215e85e5eed508317f05f6c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8edfc0b64eef30c41118",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edfc0b64eef30c41118",
+    "url": "/static/js/main.469211a5.chunk.js"
+  },
+  {
+    "revision": "10545e6b10910a0b9f12",
+    "url": "/static/js/runtime-main.025be44c.js"
+  }
+]);
